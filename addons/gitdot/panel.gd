4552797ce17a6b594c5280993c3757a6f372e328
AnonymousXC <cvxanonymous007@gmail.com>
2022-01-27 20:16:25 -1200

    Version 2.0.

--- addons/gitdot/panel.gd
+++ addons/gitdot/panel.gd
@@ -9,7 +9,17 @@ var commit_output = []
 var status_output = []
 var pull_output = []
 var self_repo_pull_output = []
+var edited_file_output = []
+var diff_output = []
+var unstage_output = []
 var pro_path = ""
+
+
+
+func _ready():
+	$Output.add_color_region("@", "", Color(255,0,0), true)
+	$Output.add_color_region("+", "", Color(0,255,0), true)
+	$Output.add_color_region("-", "", Color(0,0,255), true)
 
 
 
@@ -22,10 +32,10 @@ func _on_Initailize_pressed():
 	OS.execute("CMD.exe", ["/C",git_in], true, initailize_output)
 	for out in initailize_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Either git is not installed or you are not connected To The Internet \n"
 
 
 
@@ -43,10 +53,10 @@ func _on_Clone_Btn_pressed():
 		OS.execute("CMD.exe" , ["/C", checkout], true , clone_output)
 	for out in clone_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Folder not recognized as Local Repo. \n"
 
 
 
@@ -61,10 +71,11 @@ func _on_Add_Button_pressed():
 		OS.execute("CMD.exe" , ["/C" , add_cmd] , true, add_output)
 	for out in add_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Did'nt clone The repo. \n"
+
 
 
 func _on_Push_Btn_pressed():
@@ -73,10 +84,11 @@ func _on_Push_Btn_pressed():
 	OS.execute("CMD.exe" , ["/C", command] , true, push_output)
 	for out in push_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@ Did'nt commit any file. \n"
+
 
 
 
@@ -87,10 +99,11 @@ func _on_Commit_Button_pressed():
 	OS.execute("CMD.exe", ["/C", commit_cmd], true, commit_output)
 	for out in commit_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Did'nt staged any file. \n"
+
 
 
 func _on_Get_Status_pressed():
@@ -98,25 +111,26 @@ func _on_Get_Status_pressed():
 	OS.execute("CMD.exe", ["/C", status_cmd] , true, status_output)
 	for out in status_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Either git is not installed or you are not connected To The Internet \n"
 
 
-func _on_Pull_Button_pressed():
-	var link = $"Pull/Pull Link".text
-	var cd_cmd = "cd " + pro_path
-	var pull_cmd = "git pull " + link
-	if link:
-		OS.execute("CMD.exe", ["/C", cd_cmd], true, pull_output)
-		OS.execute("CMD.exe", ["/C", pull_cmd], true, pull_output)
-	for out in pull_output:
-		if out:
-			print(out)
-		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+
+#func _on_Pull_Button_pressed():
+#	var link = $"Pull/Pull Link".text
+#	var cd_cmd = "cd " + pro_path
+#	var pull_cmd = "git pull " + link
+#	if link:
+#		OS.execute("CMD.exe", ["/C", cd_cmd], true, pull_output)
+#		OS.execute("CMD.exe", ["/C", pull_cmd], true, pull_output)
+#	for out in pull_output:
+#		if out:
+#			print(out)
+#		else:
+#			print("Error")
+#			print("Either git is not installed or you are not connected To The Internet")
 
 
 func _on_Pull_Self_Repo_pressed():
@@ -126,7 +140,51 @@ func _on_Pull_Self_Repo_pressed():
 	OS.execute("CMD.exe", ["/C", pull], true, self_repo_pull_output)
 	for out in self_repo_pull_output:
 		if out:
-			print(out)
+			$Output.text += out + "\n"
 		else:
-			print("Error")
-			print("Either git is not installed or you are not connected To The Internet")
+			$Output.text +="@Error \n"
+			$Output.text += "@Either broken link or you are not connected To The Internet \n"
+
+
+
+func _on_Clear_Btn_pressed():
+	$Output.text = "Outputs : \n"
+
+
+func _on_Edited_File_pressed():
+	var cd = "cd " + pro_path
+	var get_files = "git diff --name-only --staged"
+	OS.execute("CMD.exe", ["/C", cd], true, edited_file_output)
+	OS.execute("CMD.exe", ["/C", get_files], true, edited_file_output)
+	for out in edited_file_output:
+		if out:
+			$Output.text += out + "\n"
+		else:
+			$Output.text +="@Error \n"
+			$Output.text += "@Did'nt staged any file. \n"
+
+
+func _on_Diff_Btn_pressed():
+	var cd = "cd " + pro_path
+	var diff = "git diff --cached"
+	OS.execute("CMD.exe", ["/C", cd], true, diff_output)
+	OS.execute("CMD.exe", ["/C", diff], true, diff_output)
+	for out in diff_output:
+		if out:
+			$Output.text += out + "\n"
+		else:
+			$Output.text +="@Error \n"
+			$Output.text += "@Did'nt staged any file. \n"
+
+
+func _on_Unstage_Btn_pressed():
+	var cd = " cd " + pro_path
+	var unstage = "git reset"
+	OS.execute("CMD.exe", ["/C", cd], true, unstage_output)
+	OS.execute("CMD.exe", ["/C", unstage], true, unstage_output)
+	for out in unstage_output:
+		if out:
+			$Output.text += out + "\n"
+		else:
+			$Output.text +="@Error \n"
+
